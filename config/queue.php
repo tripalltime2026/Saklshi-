6d66f3bff1a66ebb0d78ba32ef2e976bb1656d11
tripalltime2026 <tookokuchukhidze@gmail.com>
2026-09-08 12:29:58 +0400

    Add config/queue.php

---
+++ config/queue.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'default' => env('QUEUE_CONNECTION', 'sync'),
+    'connections' => [
+        'sync' => ['driver' => 'sync'],
+    ],
+    'failed' => [
+        'driver' => env('QUEUE_FAILED_DRIVER', 'null'),
+        'database' => env('DB_CONNECTION', 'sqlite'),
+        'table' => 'failed_jobs',
+    ],
+];
