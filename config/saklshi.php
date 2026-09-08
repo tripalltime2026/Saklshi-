3646a22d23b2caaa6b8579cacaa401939f5285f3
tripalltime2026 <tookokuchukhidze@gmail.com>
2026-09-08 19:45:50 +0400

    Switch admin auth to username login

--- config/saklshi.php
+++ config/saklshi.php
@@ -1,6 +1,6 @@
 <?php
 
 return [
-    'admin_email' => env('ADMIN_EMAIL', 'admin@sakhlshi.ge'),
+    'admin_login' => env('ADMIN_LOGIN', 'admin'),
     'admin_password' => env('ADMIN_PASSWORD'),
 ];
