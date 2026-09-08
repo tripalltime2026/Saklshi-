fb2bdf30854e3b3c7fe01bce53f016235a479324
tripalltime2026 <tookokuchukhidze@gmail.com>
2026-09-08 12:29:26 +0400

    Add config/app.php

---
+++ config/app.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+    'name' => env('APP_NAME', 'ბათუმის სახლში'),
+    'env' => env('APP_ENV', 'production'),
+    'debug' => (bool) env('APP_DEBUG', false),
+    'url' => env('APP_URL', 'http://localhost'),
+    'timezone' => env('APP_TIMEZONE', 'Asia/Tbilisi'),
+    'locale' => env('APP_LOCALE', 'ka'),
+    'fallback_locale' => env('APP_FALLBACK_LOCALE', 'en'),
+    'faker_locale' => env('APP_FAKER_LOCALE', 'en_US'),
+    'cipher' => 'AES-256-CBC',
+    'key' => env('APP_KEY'),
+    'previous_keys' => array_filter(explode(',', (string) env('APP_PREVIOUS_KEYS', ''))),
+    'maintenance' => [
+        'driver' => 'file',
+        'store' => 'database',
+    ],
+];
