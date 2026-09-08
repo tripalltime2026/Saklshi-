d40c6278d6c991e1117511dfaabe43bae82b06f2
tripalltime2026 <tookokuchukhidze@gmail.com>
2026-09-08 12:34:37 +0400

    Add resources/views/layouts/app.blade.php

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="ka">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="theme-color" content="#243b32">
+    <title>@yield('title', 'ბათუმის სახლში')</title>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    @stack('head')
+</head>
+<body class="@yield('body-class')">
+    @yield('content')
+    @stack('scripts')
+</body>
+</html>
