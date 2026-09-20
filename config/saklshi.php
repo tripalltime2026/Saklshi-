08b391e35d7fc1f32231abb750a9a8a53c6bef82
tripalltime2026 <tookokuchukhidze@gmail.com>
2026-09-20 22:15:34 +0400

    Configure SMS provider

--- config/saklshi.php
+++ config/saklshi.php
@@ -4,4 +4,9 @@ return [
     'admin_login' => env('ADMIN_LOGIN', 'admin'),
     'admin_password' => env('ADMIN_PASSWORD'),
     'default_branch_slug' => env('DEFAULT_BRANCH_SLUG', 'batumi'),
+    'sms' => [
+        'driver' => env('SMS_DRIVER', 'log'),
+        'key' => env('SMSOFFICE_KEY'),
+        'sender' => env('SMSOFFICE_SENDER', 'SAKHLSHI'),
+    ],
 ];
